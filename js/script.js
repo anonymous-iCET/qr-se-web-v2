7a3f27d6ff05936934f6d6c5abd6e7a879c40d94
Sharada Marasinha <86095574+sharada-marasinha@users.noreply.github.com>
2024-01-05 14:06:07 +0530

    change font and aliments

--- js/script.js
+++ js/script.js
@@ -2,27 +2,14 @@ let targat = document.querySelector(".testimonials");
 let targatTitles = document.querySelectorAll(".title");
 let targatImgSec = document.querySelector(".imgSection01");
 let targatImgSec2 = document.querySelector(".imgSection02");
-
-const options = {
-    threshold : 0.5,
-    rootMargin:"0px 0px 0px 0px",
-    root : null
-};
-
-
+const options = {threshold : 0.5,rootMargin:"0px 0px 0px 0px",root : null};
 const observer = new IntersectionObserver(function(entries){
-
     entries.forEach(entry =>{
-        console.log(entry.target);
-
-        const intersecting = entry.isIntersecting;
-        
-        if(intersecting){
+        if(entry.isIntersecting){
             entry.target.classList.add("opacityOn");
             entry.target.classList.add("titleOpOn");
             entry.target.classList.add("onset01");
-            entry.target.classList.add("onset02");
-            
+            entry.target.classList.add("onset02");  
         }else{
             // entry.target.classList.add("testimonials");
             // entry.target.classList.add("title");
@@ -30,13 +17,8 @@ const observer = new IntersectionObserver(function(entries){
             // entry.target.classList.add("imgSection02");
         }
     })
-
 },options);
-
-targatTitles.forEach(title=>{
-    observer.observe(title);
-})
-
+targatTitles.forEach(title=>{observer.observe(title);});
 observer.observe(targat);
 observer.observe(targatImgSec);
 observer.observe(targatImgSec2);
